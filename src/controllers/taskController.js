a941d28be254bb2b8b86d3ba6290427825606260
jparso32 <jaydenpar2006@gmail.com>
2026-04-14 23:06:58 -0400

    fixed completed query filtering

--- src/controllers/taskController.js
+++ src/controllers/taskController.js
@@ -1,22 +1,34 @@
 import * as taskService from '../services/taskService.js';
 
 export async function getTasks(req, res, next) {
-  const { completed } = req.query;
+  try {
+    const { completed } = req.query;
 
-  let completedFilter;
+    let completedFilter;
 
-  if (completed === 'true') {
-    completedFilter = true;
-  } else if (completed === 'false') {
-    completedFilter = false;
+    if (completed === 'true') {
+      completedFilter = true;
+    } else if (completed === 'false') {
+      completedFilter = false;
+    } else if (completed !== undefined) {
+      return res.status(400).json({
+        error: 'Invalid completed query parameter',
+      });
+    }
+
+    const tasks = await taskService.getAllTasks(completedFilter);
+    res.json(tasks);
+  } catch (error) {
+    next(error);
   }
-
-  const tasks = await taskService.getAllTasks(completedFilter);
-  res.json(tasks);
 }
 
 export async function createTask(req, res, next) {
-  const { title, completed } = req.body;
-  const task = await taskService.createTask({ title, completed });
-  res.status(201).json(task);
+  try {
+    const { title, completed } = req.body;
+    const task = await taskService.createTask({ title, completed });
+    res.status(201).json(task);
+  } catch (error) {
+    next(error);
+  }
 }
